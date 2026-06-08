754a616fe9d6b08961b4090e001fa720e3b48044
Muhammad Waqar <mwaqar666@yahoo.com>
2026-06-08 20:27:22 +0200

    Added kubernetes files

--- client/client.py
+++ client/client.py
@@ -1,8 +1,5 @@
-import os
-
 from webapp import app
 
 
 if __name__ == "__main__":
-    is_debug = os.environ.get("FLASK_DEBUG", "0") == "1"
-    app.run(host="0.0.0.0", port=5001, debug=is_debug)
+    app.run(host="0.0.0.0", port=5001, debug=True)
